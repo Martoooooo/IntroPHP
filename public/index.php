1a45599481991785056b2dc8e21da5595d529868
Martoooooo <martin.romao@hotmail.com>
2024-04-06 18:09:23 -0300

    Introductions, files, etc

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-include "./teste.php"; #Executa os comandos de qualquer forma, mesmo que aconteçam erros nos códigos, como um arquivo especificado que não exista
+include "./teste.php"; #Executa os comandos de qualquer forma, mesmo que aconteçam erros nos códigos, como um arquivo especificado que não exista.
 require "./teste.php"; #Não executa nada caso o arquivo não exista/ esteja com erro.
 
 include_once "./teste.php"; #Garante que o arquivo seja executado apenas uma vez
